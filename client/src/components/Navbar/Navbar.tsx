662c5c16447c1dac245a19752b652713f74587bb
ribhavsingla <ribhavsingla2166@gmail.com>
2025-03-30 17:38:28 +0530

    fixed pointer

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -42,7 +42,7 @@ function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex items-center">
-              <a onClick={() => navigate("/")} className="text-xl font-bold font-montserrat">
+              <a onClick={() => navigate("/")} className="text-xl font-bold font-montserrat cursor-pointer">
                 EventHub
               </a>
             </div>
